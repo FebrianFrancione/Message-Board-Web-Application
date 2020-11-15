5376c03be03f5cb65f870fb711549b9d9b43af8a
danon360 <fakhrdany@gmail.com>
2020-11-15 00:04:28 -0500

    simplified servlets to only forward the control to business layer

--- src/main/java/DownloadServlet.java
+++ src/main/java/DownloadServlet.java
@@ -1,5 +1,6 @@
 import com.sun.glass.ui.Size;
 import packages.DAO.DAO;
+import packages.businessLayer.MessageBoard;
 import packages.database.DBConnection;
 
 import java.io.*;
@@ -20,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DownloadServlet extends HttpServlet {
 
     // size of byte buffer to send file
-    private static final int BUFFER_SIZE = 1024 * 10;
+    private static final int BUFFER_SIZE =1024*10;
 
     // database connection settings
     private String dbURL = "jdbc:mysql://127.0.0.1:3306/messageboard?user=root";
@@ -32,87 +33,151 @@ public class DownloadServlet extends HttpServlet {
 
         //get the fileID of the file requested from file_list.jsp
         String fileID = request.getParameter("fileID");
+        MessageBoard msgObj = new MessageBoard();
 
-        Connection conn = null;
-        Statement stmt = null;
-        String fileName = null;
-        Blob blob;
+        msgObj.download(fileID, request.getServletContext(), response,BUFFER_SIZE);
 
-        try {
-
-            //get conn to DB
-            conn = DBConnection.getConnection();
-
-            System.out.println("db connected");
-
-            stmt = (Statement) conn.createStatement();
-
-            //request the file Blob and the FileName from the DB
-//            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + fileID);
-            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + fileID);
-
-            if (rs1.next()) {
-                // get the file name
-                fileName = rs1.getString("fileName");
-                //get the blob
-                blob = rs1.getBlob("file");
-
-                InputStream inputStream = blob.getBinaryStream();
-
-                ServletContext context = getServletContext();
-
-                // find the mimeType of the file . Set binary if nothing is found
-                String mimeType = context.getMimeType(fileName);
-                if (mimeType == null) {
-                    mimeType = "application/binary";
-                }
-
-                // setting the Content type header, the content length header, and the Content disposition header
-                response.setContentType(mimeType);
-                response.setContentLength(inputStream.available());     //inputStream.available() will give the file length
-                String headerValue = String.format("attachment; filename=\"%s\"", fileName);
-                response.setHeader("Content-Disposition", headerValue);
-
-                // get the servlet output stream
-                OutputStream outStream = response.getOutputStream();
-
-                //buffer size is defined at the top
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int bytesRead = -1;
-
-                //writing the the outputStream
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outStream.write(buffer, 0, bytesRead);
-                }
-
-                inputStream.close();
-                outStream.flush();
-                outStream.close();
-
-                /*
-                byte[] imgData = rs1.getBytes("file");//Here....... r1.getBytes() extract byte data from resultSet
-                System.out.println(imgData);
-                response.setHeader("expires", "0");
-                response.setContentType("image/jpg");
-
-                OutputStream os = response.getOutputStream(); // output with the help of outputStream
-                os.write(imgData);
-                os.flush();
-                os.close();*/
-
-            }
-        } catch (SQLException ex) {
-            // String message = "ERROR: " + ex.getMessage();
-            ex.printStackTrace();
-        } finally {
-            if (conn != null) {
-                // closes the database connection
-                try {
-                    conn.close();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
+//        Connection conn = null;
+//        Statement stmt = null;
+//        String fileName = null;
+//        Blob blob;
+//
+//        try {
+//
+//            //get conn to DB
+//            conn = DBConnection.getConnection();
+//
+//            System.out.println("db connected");
+//
+//            stmt = (Statement) conn.createStatement();
+//
+//            //request the file Blob and the FileName from the DB
+////            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + fileID);
+//            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + 20);
+//
+//            if (rs1.next()) {
+//                // get the file name
+//                fileName = rs1.getString("fileName");
+//                //get the blob
+//                blob = rs1.getBlob("file");
+//
+//                InputStream inputStream = blob.getBinaryStream();
+//
+//                ServletContext context = getServletContext();
+//
+//                // find the mimeType of the file . Set binary if nothing is found
+//                String mimeType = context.getMimeType(fileName);
+//                if (mimeType == null) {
+//                    mimeType = "application/binary";
+//                }
+//
+//                // setting the Content type header, the content length header, and the Content disposition header
+//                response.setContentType(mimeType);
+//                response.setContentLength(inputStream.available());     //inputStream.available() will give the file length
+//                String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+//                response.setHeader("Content-Disposition" , headerValue);
+//
+//                // get the servlet output stream
+//                OutputStream outStream = response.getOutputStream();
+//
+//                //buffer size is defined at the top
+//                byte[] buffer = new byte[BUFFER_SIZE];
+//                int bytesRead = -1;
+//
+//                //writing the the outputStream
+//                while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                    outStream.write(buffer, 0, bytesRead);
+//                }
+//
+//                inputStream.close();
+//                outStream.flush();
+//                outStream.close();
+//
+//                /*
+//                byte[] imgData = rs1.getBytes("file");//Here....... r1.getBytes() extract byte data from resultSet
+//                System.out.println(imgData);
+//                response.setHeader("expires", "0");
+//                response.setContentType("image/jpg");
+//
+//                OutputStream os = response.getOutputStream(); // output with the help of outputStream
+//                os.write(imgData);
+//                os.flush();
+//                os.close();*/
+//
+//            }
+//        } catch (SQLException ex) {
+//            // String message = "ERROR: " + ex.getMessage();
+//            ex.printStackTrace();
+//        } finally {
+//            if (conn != null) {
+//                // closes the database connection
+//                try {
+//                    conn.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
     }
+/*
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int postID = Integer.parseInt(request.getParameter("postId"));
+            int attachmentID = Integer.parseInt(request.getParameter("attachmentId"));
+
+            DAO upd = new DAO();
+            String fileName = upd.getFileName(postID, attachmentID);
+            response.setContentType(upd.getContentType(postID, attachmentID));
+            response.setHeader("Content-Disposition", "attachment; fileName= " + fileName);
+
+            OutputStream out = response.getOutputStream();
+
+            upd.downloadFile(postID, attachmentID, fileName, out);
+
+
+        }*/
+
+//        Blob image = null;
+//        Connection con = null;
+//        Statement stmt = null;
+//        ResultSet rs = null;
+//        ServletOutputStream out = response.getOutputStream();
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/messageboard?user=root","root","1234");
+//            stmt = con.createStatement();
+//            rs = stmt.executeQuery("select file from files where fileID = '1'");
+//            if (rs.next()) {
+//                image = rs.getBlob(1);
+//            } else {
+//                response.setContentType("text/html");
+//                out.println("<html><head><title>Display Blob Example</title></head>");
+//                out.println("<body><h4><font color='red'>image not found for given id</font></h4></body></html>");
+//                return;
+//            }
+//            response.setContentType("image/gif");
+//            InputStream in = image.getBinaryStream();
+//            int length = (int) image.length();
+//            int bufferSize = 1024;
+//            byte[] buffer = new byte[bufferSize];
+//            while ((length = in.read(buffer)) != -1) {
+//                out.write(buffer, 0, length);
+//            }
+//            in.close();
+//            out.flush();
+//        } catch (Exception e) {
+//            response.setContentType("text/html");
+//            out.println("<html><head><title>Unable To Display image</title></head>");
+//            out.println("<body><h4><font color='red'>Image Display Error=" + e.getMessage() +
+//                    "</font></h4></body></html>");
+//            return;
+//        } finally {
+//            try {
+//                rs.close();
+//                stmt.close();
+//                con.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//
 }
+
--- src/main/java/packages/businessLayer/MessageBoard.java
+++ src/main/java/packages/businessLayer/MessageBoard.java
@@ -3,6 +3,8 @@ package packages.businessLayer;
 import packages.DAO.DAO;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import javax.swing.*;
 import javax.xml.bind.DatatypeConverter;
@@ -13,14 +15,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 //import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import org.apache.commons.codec.binary.Base64;
+import packages.database.DBConnection;
 
 
 public class MessageBoard {
@@ -57,17 +58,17 @@ public class MessageBoard {
     }
 
     public void deletePost(int userID, int postID) {
-        daoObj.deletePost(userID ,postID);
+        daoObj.deletePost(userID, postID);
     }
 
     public void updatePost(int userID, int postID, String text, InputStream inputStream, String tags) {
         daoObj.update(userID, postID, text, inputStream, tags);
     }
 
-    public ArrayList<Post> searchCases(String option, String username,String fromDate, String toDate, String tag){
+    public ArrayList<Post> searchCases(String option, String username, String fromDate, String toDate, String tag) {
 
-        if (option instanceof String){
-            switch (option){
+        if (option instanceof String) {
+            switch (option) {
                 case "searchUser":
                     return daoObj.searchPosts(username);
                 case "searchByDate":
@@ -82,7 +83,7 @@ public class MessageBoard {
     }
 
     // display searched items
-    public String search(String option, String username,String fromDate, String toDate, String tag, int i, String reverse) throws IOException {
+    public String search(String option, String username, String fromDate, String toDate, String tag, int i, String reverse) throws IOException {
         System.out.println(option + username + fromDate + toDate + tag + i + reverse);
         String out = "";
 
@@ -96,13 +97,14 @@ public class MessageBoard {
 
         String lastUpdated = "";
 
-        if(searchedAllPosts != null){
+        if (searchedAllPosts != null) {
             if (reverse.equals("true")) {
                 Collections.reverse(filesSearchedAllPosts);
             }
         }
 
-        if(searchedAllPosts != null){  int counter = 0;
+        if (searchedAllPosts != null) {
+            int counter = 0;
             for (Post post : filesSearchedAllPosts) {
                 if (counter == i) {
                     break;
@@ -133,7 +135,8 @@ public class MessageBoard {
                     out += "</div>";
                 }
                 counter++;
-            }}
+            }
+        }
         return out;
     }
 
@@ -152,7 +155,7 @@ public class MessageBoard {
         }
 
         int counter = 0;
-        for (Post post: postsList) {
+        for (Post post : postsList) {
             if (counter == i) {
                 break;
             }
@@ -163,10 +166,10 @@ public class MessageBoard {
 
             out += "" +
                     "<div class=\"post\" style=\"margin: 0 20px; padding: 10px;\" id=" + post.getUserID() + ">" +
-                    "<div class=\"post-ids\" style=\"display: flex; flex-direction: row; justify-content: space-between;\">"+
+                    "<div class=\"post-ids\" style=\"display: flex; flex-direction: row; justify-content: space-between;\">" +
                     "<div>Username: " + daoObj.retrieveUsername(post.getUserID()) + "</div>" +
                     "<div>Post id: " + post.getPostID() + "</div>" +
-                    "</div>"+
+                    "</div>" +
                     "<div class=\"post-body\" style=\"font-size: 20px; margin-top: 20px;\">" + post.getText() + "</div>" +
                     "<div class=\"post-tags\" style=\"margin-top: 20px; color: grey;\">" + post.getTags() + "</div>" +
                     "<div class=\"post-date\" style=\"margin-top: 10px; font-size: 12px;\">" + post.getDate().toString() + "<br>" + lastUpdated + "</div>";
@@ -174,27 +177,27 @@ public class MessageBoard {
             BufferedImage image = null;
 
             if (post.getAttachment() != null) {
-                byte[] imageBytes = new byte[(int)post.getAttachment().available()];
+                byte[] imageBytes = new byte[(int) post.getAttachment().available()];
                 post.getAttachment().read(imageBytes, 0, imageBytes.length);
                 String imageString = Base64.encodeBase64String(imageBytes);
                 out += "<div style=\"margin-top: 15px;\"><img style=\"width:200px; height: 250px;\" src=\"data:image/jpeg;base64, " + imageString + "\"></div></div>";
-            }else {
+            } else {
                 out += "</div>";
             }
-            counter ++;
+            counter++;
         }
         return out;
     }
 
     public ArrayList<Integer> retrievePostIDs() {
         ArrayList<Integer> IDs = new ArrayList<Integer>();
-        for (Post post: daoObj.retrievePosts()) {
+        for (Post post : daoObj.retrievePosts()) {
             IDs.add(post.getPostID());
         }
         return IDs;
     }
 
-    public int verifyUser(String username, String password){
+    public int verifyUser(String username, String password) {
 
         MessageDigest digest = null;
         try {
@@ -208,13 +211,97 @@ public class MessageBoard {
 
         //converting byte array to String of hex
         String hashedpass = DatatypeConverter.printHexBinary(encodedhash);
-        User temp = new User(username,hashedpass);
+        User temp = new User(username, hashedpass);
 
         User returned = daoObj.verifyPassword(temp);
 
-        if (returned != null){
+        if (returned != null) {
             return returned.getUserID();
         }
         return -1;
+    }
+
+    public void download(String fileID, ServletContext context, HttpServletResponse response, int BufferSize) {
+        Connection conn = null;
+        Statement stmt = null;
+        String fileName = null;
+        Blob blob;
+
+        try {
+
+            //get conn to DB
+            conn = DBConnection.getConnection();
+
+            System.out.println("db connected");
+
+            stmt = (Statement) conn.createStatement();
+
+            //request the file Blob and the FileName from the DB
+//            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + fileID);
+            ResultSet rs1 = stmt.executeQuery("select file,fileName from files where fileID = " + fileID);
+
+            if (rs1.next()) {
+                // get the file name
+                fileName = rs1.getString("fileName");
+                //get the blob
+                blob = rs1.getBlob("file");
+
+                InputStream inputStream = blob.getBinaryStream();
+
+                //ServletContext context = getServletContext();
+
+                // find the mimeType of the file . Set binary if nothing is found
+                String mimeType = context.getMimeType(fileName);
+                if (mimeType == null) {
+                    mimeType = "application/binary";
+                }
+
+                // setting the Content type header, the content length header, and the Content disposition header
+                response.setContentType(mimeType);
+                response.setContentLength(inputStream.available());     //inputStream.available() will give the file length
+                String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+                response.setHeader("Content-Disposition", headerValue);
+
+                // get the servlet output stream
+                OutputStream outStream = response.getOutputStream();
+
+                //buffer size is defined at the top
+                byte[] buffer = new byte[BufferSize];
+                int bytesRead = -1;
+
+                //writing the the outputStream
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outStream.write(buffer, 0, bytesRead);
+                }
+
+                inputStream.close();
+                outStream.flush();
+                outStream.close();
+
+                /*
+                byte[] imgData = rs1.getBytes("file");//Here....... r1.getBytes() extract byte data from resultSet
+                System.out.println(imgData);
+                response.setHeader("expires", "0");
+                response.setContentType("image/jpg");
+
+                OutputStream os = response.getOutputStream(); // output with the help of outputStream
+                os.write(imgData);
+                os.flush();
+                os.close();*/
+
+            }
+        } catch (SQLException | IOException ex) {
+            // String message = "ERROR: " + ex.getMessage();
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                // closes the database connection
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
